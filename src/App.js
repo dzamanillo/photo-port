ce070a4c831d28cb54a93b030822d62a99fdb865
Daniel Zamanillo <dzamanillok@gmail.com>
2021-10-19 13:18:09 -0500

    nav and about tested

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
-import About from "./components/About";
-import Nav from "./components/Nav";
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
-	return (
-		<div>
-			<Nav></Nav>
-			<main>
-				<About></About>
-			</main>
-		</div>
-	);
+
+  return (
+    <div>
+      <Nav />
+      <main>
+          <About />        
+      </main>
+    </div>
+  );
 }
 
 export default App;

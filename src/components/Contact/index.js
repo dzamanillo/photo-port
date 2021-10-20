4002dbd6bea43d03dbdb8ad0e955f18ef1dcf10a
Daniel Zamanillo <dzamanillok@gmail.com>
2021-10-20 16:35:11 -0500

    form functionallity added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+	const [formState, setFormState] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
+
+	const { name, email, message } = formState;
+
+	function handleChange(event) {
+		setFormState({ ...formState, [event.target.name]: event.target.value });
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		console.log(formState);
+	}
+
 	return (
 		<section>
 			<h1>Contact Me</h1>
-			<form id="contact-form">
+			<form id="contact-form" onSubmit={handleSubmit}>
 				<div>
 					<label htmlFor="name">Name:</label>
-					<input type="text" name="name" />
+					<input
+						type="text"
+						name="name"
+						defaultValue={name}
+						onChange={handleChange}
+					/>
 				</div>
 				<div>
 					<label htmlFor="email">Email Address:</label>
-					<input type="email" name="email" />
+					<input
+						type="email"
+						name="email"
+						defaultValue={email}
+						onChange={handleChange}
+					/>
 				</div>
 				<div>
 					<label htmlFor="message">Message:</label>
-					<textarea name="message" rows="5"></textarea>
+					<textarea
+						name="message"
+						rows="5"
+						defaultValue={message}
+						onChange={handleChange}
+					></textarea>
 				</div>
 				<button type="submit">Submit</button>
 			</form>
